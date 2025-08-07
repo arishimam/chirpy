b22fd7f9f1eb60082fa634dcacb3a2088d258176
Arish Imam <arish.imam@gmail.com>
2025-08-07 10:40:04 -0700

    metrics and reset paths added

--- main.go
+++ main.go
@@ -1,28 +1,36 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"log"
 	"net/http"
-	// "path"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
-	const port = ":8080"
-	const filepath = "."
+	const port = "8080"
+	const filepathRoot = "."
+
+	apiCfg := apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepath))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
 
 	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 
 	server := http.Server{
 		Handler: mux,
-		Addr:    port,
+		Addr:    ":" + port,
 	}
 
-	log.Printf("Serving files from %s on port %s\n", filepath, port)
-	log.Fatalf("%w", server.ListenAndServe())
+	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,4 +38,23 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = atomic.Int32{}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0!"))
 }
