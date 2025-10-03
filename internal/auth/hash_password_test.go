11c89ce047b5e366713cac622029793a1c2fdd69
Arish Imam <arish.imam@gmail.com>
2025-10-03 11:10:47 -0700

    password test ip

--- internal/auth/hash_password_test.go
+++ internal/auth/hash_password_test.go
@@ -1,6 +1,22 @@
 package auth
 
 import (
-	"regexp"
 	"testing"
 )
+
+func TestHashPassword(t *testing.T) {
+	password := "tester"
+	hashed_password, err := HashPassword(password)
+	if err != nil {
+		t.Errorf("Error occured when called HashPassword function")
+	}
+
+	if hashed_password == "" || hashed_password == "tester" {
+		t.Errorf("Password was not successfully hashed!\nHashed password = %v", hashed_password)
+	}
+
+}
+
+func TestCheckPasswordHash(t *testing.T) {
+
+}
