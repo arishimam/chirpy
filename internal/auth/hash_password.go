5110a3d4810df494dea388fdb5b4dc613d177ce7
Arish Imam <arish.imam@gmail.com>
2025-10-03 11:00:34 -0700

    passwords ip

---
+++ internal/auth/hash_password.go
@@ -0,0 +1,22 @@
+package auth
+
+import "github.com/alexedwards/argon2id"
+
+func HashPassword(password string) (string, error) {
+	hashedPass, err := argon2id.CreateHash(password)
+	if err != nil {
+		return "", err
+
+	}
+
+	return hashedPass, nil
+}
+
+func CheckPasswordHash(password, hash string) (bool, error) {
+	match, err := argon2id.ComparePasswordAndHash(password, hash)
+	if err != nil {
+		return false, err
+	}
+
+	return match, nil
+}
