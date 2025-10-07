c1cf4ae6ea741e486cc33bb0be2e6fedc799724b
Arish Imam <arish.imam@gmail.com>
2025-10-07 16:00:39 -0700

    need to add post /api/login and test

--- internal/auth/hash_password_test.go
+++ internal/auth/hash_password_test.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -18,5 +19,21 @@ func TestHashPassword(t *testing.T) {
 }
 
 func TestCheckPasswordHash(t *testing.T) {
+	test := "hello"
+	pass1, err := HashPassword(test)
+	if err != nil {
+		t.Errorf("Error occured when calling HashPassword function")
+	}
+
+	pass2, err := HashPassword(test)
+	if err != nil {
+		t.Errorf("Error occured when calling HashPassword function")
+	}
+
+	if pass1 == pass2 {
+		t.Errorf("pass1 and pass2 should be hashed to different passwords")
+	}
+	//fmt.Println("pass1 ", pass1)
+	//fmt.Println("pass2 ", pass2)
 
 }
